13c56119cbcdda27094198d6ff269a74ac508ee1
Developerts <developerts@MacBook-Pro-de-Developert.local>
2026-03-31 19:10:41 +0200

    31-03-2026

--- platform/plugins/ecommerce/resources/views/orders/partials/back-to-cart.blade.php
+++ platform/plugins/ecommerce/resources/views/orders/partials/back-to-cart.blade.php
@@ -1,3 +1,3 @@
-<a class="d-flex align-items-center gap-1 payment-checkout-btn-back " href="{{ route('public.cart') }}">
+<a class="btn payment-checkout-btn-back w-100" href="{{ route('public.cart') }}">
     <span class="d-inline-block back-to-cart">{{ __('Back to cart') }}</span>
 </a>
